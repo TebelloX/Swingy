b0d8762cfa1527e8bffe2d049749ba2ad49e6f7a
TebelloX <tebellonamo@gmail.com>
2019-11-05 11:21:19 +0200

    added user input

--- swingy/src/main/java/swingy/App.java
+++ swingy/src/main/java/swingy/App.java
@@ -1,13 +1,22 @@
-package swingy;
-
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.Scanner;
+public class App
 {
-    public static void main( String[] args )
+  public static void main (String[]args)
+  {
+    int countIt = 0;
+    Scanner myLimit = new Scanner (System.in);
+
+    System.out.println ("What's your limit? ");
+    int actualLimit;
+    actualLimit = myLimit.nextInt ();
+    while (true)
     {
-        System.out.println( "Hello World!" );
+	    countIt++;
+	    if (countIt == actualLimit)
+	    {
+	        System.out.println ("Your hero dies");
+	        break;
+	    }
     }
+  }
 }
